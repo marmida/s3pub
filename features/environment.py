0e8bbe376b481476c7124ea96035b983d4cc95c6
Michael Rene Armida <me@marmida.com>
2014-12-16 22:55:44 -0800

    wip test development

--- features/environment.py
+++ features/environment.py
@@ -5,8 +5,11 @@ Environment setup for s3pub behave tests.
 from __future__ import absolute_import
 
 from boto.s3.connection import S3Connection
+import hashlib
 import os.path
+import random
 import requests
+import tempfile
 import types
 import yaml
 import yurl
@@ -14,17 +17,52 @@ import yurl
 CONFIG_PATH = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'test_config.yaml')
 
+def rand_token():
+    '''
+    Generate a string of random hex chars.
+    '''
+    h = hashlib.sha1()
+    h.update(str(random.random()))
+    return h.hexdigest()[:10]
+
+def rand_path(subdirs=0):
+    '''
+    Return a path of randomized components.
+    '''
+    return '/'.join([rand_token()] + [rand_token() for _ in xrange(subdirs)])
+
+class TempFileManager(object):
+    def __init__(self):
+        self.root = None
+        self.registry = {}
+
+    def __enter__(self):
+        self.root = tempfile.mkdtemp()
+        return self
+
+    def __exit__(self, exc_type, exc_val, traceback):
+        shutil.rmtree(self.root)
+
+    def create(self, subdirs=0):
+        path = rand_path(subdirs)
+        self.registry[path] = rand_token()
+        with open(os.path.join(self.root, path), 'w') as temp_fp:
+            temp_fp.write(self.registry[path])
+
+    def __iter__(self):
+        return self.registry.iteritems()
+
+    def iterkeys(self):
+        return self.__iter__()
+
 def before_all(context):
     with open(CONFIG_PATH) as config_fp:
         context.config = yaml.safe_load(config_fp)
     context.base_url = yurl.URL(context.config['s3-url'])
 
     def request(self, path):
-        return requests.get(
-            self.base_url.replace(path=path).as_string()).status_code
+        return requests.get(self.base_url.replace(path=path).as_string())
     context.request = types.MethodType(request, context)
 
-    def connect_s3(self):
-        return S3Connection(
-            context.config['access-key'], context.config['secret-access-key'])
-    context.connect_s3 = types.MethodType(connect_s3, context)
+    context.bucket = context.connect_s3().get_bucket(context.config['bucket'])
+    
--- features/steps/s3pub.py
+++ features/steps/s3pub.py
@@ -4,21 +4,29 @@ Step implementations.
 
 from __future__ import absolute_import
 
+from behave import given, when, then
+from nose.tools import assert_equal
 
+import s3pub.upload
 
 @given('that no files exist in the bucket')
 def init_empty_bucket(context):
-    conn = context.connect_s3()
-    bucket = conn.get_bucket(context.config['bucket'])
-    bucket.delete_keys([key.name for key in bucket.list()])
+    context.bucket.delete_keys([key.name for key in context.bucket.list()])
 
 @when('I publish new test content to the bucket')
 def init_content_and_publish(context):
-    assert False
+    context.tempfiles.create()
+    context.tempfiles.create(subdirs=1)
+    context.tempfiles.create(subdirs=2)
+    s3pub.upload.do_upload(
+        context.tempfiles.root, context.config['bucket'], True)
 
 @then('the test content should be available from the bucket\'s S3 URL')
 def assert_s3_web(context):
-    assert False
+    for path, content in context.tempfiles:
+        resp = context.request(path)
+        resp.raise_for_status()
+        assert_equal(content, resp.text)
 
 @given('that previous test content exists in the bucket')
 def init_non_empty_bucket(context):
