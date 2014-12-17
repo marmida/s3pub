856d49bf763de5e85e3638773cbe780e55740ad1
Michael Rene Armida <me@marmida.com>
2014-12-16 23:10:15 -0800

    Tests are failing in S3 now because s3pub doesn't support arbitrary keys.

--- features/environment.py
+++ features/environment.py
@@ -6,9 +6,11 @@ from __future__ import absolute_import
 
 from boto.s3.connection import S3Connection
 import hashlib
+import os
 import os.path
 import random
 import requests
+import shutil
 import tempfile
 import types
 import yaml
@@ -32,24 +34,41 @@ def rand_path(subdirs=0):
     return '/'.join([rand_token()] + [rand_token() for _ in xrange(subdirs)])
 
 class TempFileManager(object):
+    '''
+    Create temporary files and directories with random names and content.
+
+    Remove them all on exit.
+    '''
     def __init__(self):
-        self.root = None
         self.registry = {}
-
-    def __enter__(self):
         self.root = tempfile.mkdtemp()
-        return self
 
-    def __exit__(self, exc_type, exc_val, traceback):
+    def cleanup(self):
+        '''
+        Remove all temporary files on disk.
+        '''
         shutil.rmtree(self.root)
+        self.registry = {}
+        self.root = None
 
     def create(self, subdirs=0):
+        '''
+        Create a new file with random content and a random filename.
+
+        If subdirs > 0, add that many layers of directories with random names.
+        '''
         path = rand_path(subdirs)
+        temp_path = os.path.join(self.root, path)
+        if subdirs:
+            os.makedirs(os.path.dirname(temp_path))
         self.registry[path] = rand_token()
-        with open(os.path.join(self.root, path), 'w') as temp_fp:
+        with open(temp_path, 'w') as temp_fp:
             temp_fp.write(self.registry[path])
 
     def __iter__(self):
+        '''
+        Return an iterable of (path, contents) tuples for all temp files.
+        '''
         return self.registry.iteritems()
 
     def iterkeys(self):
@@ -64,5 +83,12 @@ def before_all(context):
         return requests.get(self.base_url.replace(path=path).as_string())
     context.request = types.MethodType(request, context)
 
-    context.bucket = context.connect_s3().get_bucket(context.config['bucket'])
+    context.connection = S3Connection(
+        context.config['access-key'], context.config['secret-access-key'])
+    context.bucket = context.connection.get_bucket(context.config['bucket'])
     
+def before_scenario(context, _):
+    context.tempfiles = TempFileManager()
+
+def after_scenario(context, _):
+    context.tempfiles.cleanup()
